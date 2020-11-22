d663cbbb3729511b0ad17a87dc8445113a883dca
Ahmad El Gamal <ahmadelgamal@gmail.com>
2020-11-21 19:29:48 -0800

    Adds server with route to root and package.json with dotenv

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
